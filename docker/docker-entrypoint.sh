d9fa9b75da944bb2437d95d4e02de7aa7990e860
sid2021 <wolanski.jakub@gmail.com>
2022-02-13 19:34:39 +0100

    Added Dockerfile.heroku

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,11 +9,4 @@ runserver)
   python manage.py runserver 0.0.0.0:8001
   ;;
 
-gunicorn)
-  shift
-  python manage.py collectstatic --noinput
-  python manage.py migrate
-  python gunicorn config.wsgi -b 0.0.0.0:8001
-  ;;
-
 esac
